5803e4d816d0e6b1479d5f0387df0329609e0f10
Vitaly Ulchenkov <v.ulchenkov@outlook.com>
2021-12-24 11:22:19 -0500

    Added test project

---
+++ Test/Program.cs
@@ -0,0 +1,6 @@
+﻿// example of boolean expression parser: https://www.meziantou.net/creating-a-parser-for-boolean-expressions.htm
+
+// https://stackoverflow.com/questions/1669/learning-to-write-a-compiler
+
+
+string str = "(2 > 3 || ( 6 = 6 && 10 > 9))";
