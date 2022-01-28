ad6e3ecafe23f2f191573c7609a19e65018fd2b8
Vitaly Ulchenkov <v.ulchenkov@outlook.com>
2022-01-28 05:27:52 -0500

    started NewHtmlTable

---
+++ PSCmdlets/NewHtmlTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data;
+using System.Management.Automation;
+
+namespace ETL
+{
+    [Cmdlet(VerbsCommon.New, "HtmlTable")]
+    [Alias("nht")]
+    internal class NewHtmlTable : PSCmdlet
+    {
+        [Parameter(Position = 0, Mandatory = false)]
+        public int Limit { get; set; } = 10;
+    }
+}
