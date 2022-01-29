9998448aaeb9a8a502a90a46488318c06a4a56b1
Vitaly Ulchenkov <github@ulchenkov.com>
2022-01-29 15:11:00 -0500

    add first version of GetHtmlTable

--- PSCmdlets/NewHtmlTable.cs
+++ PSCmdlets/NewHtmlTable.cs
@@ -1,14 +1,29 @@
 ﻿using System;
 using System.Data;
 using System.Management.Automation;
+using System.Xml.Linq;
 
 namespace ETL
 {
-    [Cmdlet(VerbsCommon.New, "HtmlTable")]
-    [Alias("nht")]
+    [Cmdlet(VerbsCommon.Get, "HtmlTable")]
+    [Alias("ght")]
     internal class NewHtmlTable : PSCmdlet
     {
-        [Parameter(Position = 0, Mandatory = false)]
-        public int Limit { get; set; } = 10;
+        [Parameter(Position = 0, Mandatory = true)]
+        public DataTable dataTable { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            foreach (DataRow row in dataTable.Rows)
+            {
+                string output = "";
+                foreach (DataColumn column in dataTable.Columns)
+                {
+                    output = $"{output} | {row[column]}";
+                }
+                WriteDebug(output);
+
+            }
+        }
     }
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -2,5 +2,18 @@
 
 // https://stackoverflow.com/questions/1669/learning-to-write-a-compiler
 
+using HtmlTags;
 
-string str = "(2 > 3 || ( 6 = 6 && 10 > 9))";
+//string str = "(2 > 3 || ( 6 = 6 && 10 > 9))";
+
+HtmlTag tag = new HtmlTag("div");
+
+var tag1 = new HtmlTag("span")
+    .Text("Hello & Goodbye")
+    .AddClass("important")
+    .AddClass("import")
+    .Attr("title", "Greeting");
+
+tag.Append(tag1);
+
+Console.WriteLine(tag.ToString());
